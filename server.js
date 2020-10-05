ff70efc04199ba91827df74035b26e4b4fd9aee6
Afshin Rahmati <afshinrahmati93@gmail.com>
2020-10-05 12:10:26 -0400

    xx

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 const express = require('express');
 global.Config = require("./Config");
+const Kavenegar = require('kavenegar');
 const RouterIndex = require('./Routers/IndexRouter')
-const app= express();
-
-//set 
-app.use(express.static(__dirname+"/public"));
-app.set('view engine','ejs');
+const app = express();
+const mongoose = require('mongoose')
+    //set 
+app.use(express.static(__dirname + "/public"));
+app.set('view engine', 'ejs');
 
 
 
 
 
 // Read Router
-app.use('/',RouterIndex);
+app.use('/', RouterIndex);
 
 
 
@@ -38,8 +39,9 @@ app.use('/',RouterIndex);
 
 
 
+mongoose.connect('mongodb://localhost:27017/Yajoo', { useNewUrlParser: true, useUnifiedTopology: true });
 
 
-app.listen(Config.Port,()=>{
+app.listen(Config.Port, () => {
     console.log(`Server ${Config.Port} Run`);
 })
