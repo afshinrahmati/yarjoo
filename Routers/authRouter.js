722971840a173755570e7041b550d87a8ebcd0d6
Afshin Rahmati <afshinrahmati93@gmail.com>
2020-10-09 13:26:14 -0400

    shows

--- Routers/authRouter.js
+++ Routers/authRouter.js
@@ -6,6 +6,10 @@ router.get('/Regester', AuthController.RegesterGet);
 
 router.post("/Regester1", ValidatorAuth.Regester(), AuthController.PostRegestedr);
 
+// **code Rigth
+
+router.post('/code-rigth/:id', AuthController.Rigth_Code_post)
+
 
 
 
--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -8,36 +8,25 @@ module.exports = new class DashboardController extends Controllr {
     async RegesterGet(req, res, next) {
             try {
                 let a = await User.find({ moblie: req.flash("me") });
-
-
-
                 if (a.length === 1) {
                     let me_code = a[0].expiencode;
-
                     let x1 = momment().add(3, 'minutes');
                     var x = setInterval(function() {
-
                         var distance = me_code - momment();
                         // console.log(typeof(distance));
                         var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
                         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
                         let o = minutes + ":" + seconds;
-                        console.log(o);
-
+                        // console.log(o);
                         if (distance < 0) {
                             clearInterval(x);
                             return res.redirect('/user/Regester')
                         }
+                    }, 1000);
 
-                    }, 1000)
-
-
-                    return res.render('auth/add-code.ejs')
+                    return res.render('auth/add-code.ejs', { user: a })
                 }
 
-
-                //if mobile bod
                 return res.render('auth/Regester.ejs', { error: req.flash("errorsValidator"), Threris: req.flash("Threis") })
             } catch (error) {
 
@@ -45,43 +34,58 @@ module.exports = new class DashboardController extends Controllr {
         }
         //Post Regester
     async PostRegestedr(req, res, next) {
-        try {
+            try {
 
-            const us = await User.findOne({
-                moblie: req.body.mobile
-            });
-            if (us) {
-                req.flash("Threis", "شما قبلا ثبت نام کرده اید.")
+                const us = await User.findOne({
+                    moblie: req.body.mobile
+                });
+                if (us) {
+                    req.flash("Threis", "شما قبلا ثبت نام کرده اید.")
+                    return res.redirect("/user/Regester");
+                }
+
+
+                //mmone now this min +3
+                let dateshow = momment().add(3, 'minutes');
+
+                let Code = Math.floor(10000 + Math.random() * 90000);
+
+                const errors = validationResult(req);
+                if (!errors.isEmpty()) {
+                    let MyErrors = errors.array().map(err => err.msg);
+                    req.flash("errorsValidator", MyErrors);
+                    return res.redirect("/user/Regester");
+                };
+
+                const NewUser = new User({
+                    moblie: req.body.mobile,
+                    active: 1,
+                    expiencode: momment().add(3, 'minutes'),
+                    code: Code,
+                });
+
+                await NewUser.save();
+
+                req.flash("me", req.body.mobile)
                 return res.redirect("/user/Regester");
+            } catch (e) {
+
             }
+        }
+        // Rigth Code
+    async Rigth_Code_post(req, res, next) {
+        try {
+            let a = await User.findById({ _id: req.params.id });
+            let find_user = await User.findById({ _id: req.params.id });
 
-
-            //mmone now this min +3
-            let dateshow = momment().add(3, 'minutes');
-
-            let Code = Math.floor(10000 + Math.random() * 90000);
-
-            const errors = validationResult(req);
-            if (!errors.isEmpty()) {
-                let MyErrors = errors.array().map(err => err.msg);
-                req.flash("errorsValidator", MyErrors);
-                return res.redirect("/user/Regester");
-            };
-
-            const NewUser = new User({
-                moblie: req.body.mobile,
-                active: 1,
-                expiencode: momment().add(3, 'minutes')
-
-
-            });
-
-            await NewUser.save();
-
-            req.flash("me", req.body.mobile)
-            return res.redirect("/user/Regester");
+            if (find_user.code === req.body.Code) {
+                return res.send(`Welcome ${find_user.mobile}`)
+            }
+            req.flash("Error_code", "متاسفانه کد ارسال شده همخوانی ندارد");
+            return res.render('auth/add-code.ejs', { user: a, Error: req.flash("Error_code") });
         } catch (e) {
 
         }
     }
+
 }
